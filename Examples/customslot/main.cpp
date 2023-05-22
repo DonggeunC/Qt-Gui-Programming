f5c0341ce31bcd2f6dce4b065d39580f2acdd76f
DonggeunC <124149731+DonggeunC@users.noreply.github.com>
2023-05-22 11:57:54 +0900

    Update main.cpp

--- Examples/customslot/main.cpp
+++ Examples/customslot/main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char** argv)
     QApplication app(argc, argv);
 
     CustomWidget *widget = new CustomWidget(0);
+    QObject::connect(widget,SIGNAL(widgetClicked()),&app,SLOT(quit()));
     widget->show(); 		/* 위젯의 화면 표시 */
 
     return app.exec();
